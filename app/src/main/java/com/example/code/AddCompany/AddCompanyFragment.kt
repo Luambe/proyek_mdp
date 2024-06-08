8a68f9dcdbac151b65709b0bee7d4f1b22e8291d
Revereign <tenata4321@gmail.com>
2024-06-08 21:51:41 +0700

    front end add company

---
+++ app/src/main/java/com/example/code/AddCompany/AddCompanyFragment.kt
@@ -0,0 +1,32 @@
+package com.example.code.AddCompany
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.code.R
+
+class AddCompanyFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = AddCompanyFragment()
+    }
+
+    private lateinit var viewModel: AddCompanyViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_add_company, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(AddCompanyViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/code/AddCompany/AddCompanyViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.code.AddCompany
+
+import androidx.lifecycle.ViewModel
+
+class AddCompanyViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
