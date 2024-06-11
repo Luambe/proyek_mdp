254c83cdf65aec44726d5f9ce09c13a3d21d5405
NickDabizaz <94162428+NickDabizaz@users.noreply.github.com>
2024-06-11 21:54:57 +0700

    Harusnya viewmodel add task beres

--- app/src/main/java/com/example/code/AddTask/AddTaskViewModel.kt
+++ app/src/main/java/com/example/code/AddTask/AddTaskViewModel.kt
@@ -1,7 +1,33 @@
 package com.example.code.AddTask
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.code.ManageApp
+import com.example.code.data.source.model.Task
+import kotlinx.coroutines.launch
 
 class AddTaskViewModel : ViewModel() {
+    private val taskRepository = ManageApp.taskRepository
+    private val _status = MutableLiveData<String?>(null)
+    private val _task = MutableLiveData<Task?>(null)
 
+    val task: LiveData<Task?>
+        get() = _task
+
+    val status: LiveData<String?>
+        get() = _status
+
+    fun getAllTask(
+        taskName: String,
+        taskDescription: String,
+        employeeId: String,
+        managerId: String,
+        taskStatus: Int
+    ){
+        viewModelScope.launch {
+            _task.postValue(taskRepository.createTask(taskName, taskDescription, employeeId, managerId, taskStatus))
+        }
+    }
 }
