685b9d7c2019fb070f52c9fe9c59f43a7be43fb5
Revereign <tenata4321@gmail.com>
2024-06-11 21:42:55 +0700

    Push part 482637123

--- app/src/main/java/com/example/code/CompanyDashboard/CompanyDashboardFragment.kt
+++ app/src/main/java/com/example/code/CompanyDashboard/CompanyDashboardFragment.kt
@@ -2,13 +2,17 @@ package com.example.code.CompanyDashboard
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
 import androidx.cardview.widget.CardView
+import androidx.core.view.isVisible
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.navigation.fragment.findNavController
@@ -22,9 +26,14 @@ class CompanyDashboardFragment : Fragment() {
     lateinit var card_attendance: CardView
     lateinit var card_task:CardView
     lateinit var card_employee:CardView
-    lateinit var tvAnnoucement: TextView
+    lateinit var tvAnnouncement: TextView
+    lateinit var etAnnouncement: EditText
+    lateinit var btnEdit: Button
+    lateinit var btnCencel: Button
+
     var announcement_text: String = ""
     var companyId: String = "bc0f9eeb-09d3-4d31-9a61-1dcb328de5a9"
+    var nowedit: Boolean = false
     val viewModel:CompanyDashboardViewModel by viewModels<CompanyDashboardViewModel>()
 //    val navArgs:CompanyDashboardArgs by navArgs()
 
@@ -42,12 +51,19 @@ class CompanyDashboardFragment : Fragment() {
         card_attendance = view.findViewById(R.id.card_attendance)
         card_task = view.findViewById(R.id.card_task)
         card_employee = view.findViewById(R.id.card_employee)
-        tvAnnoucement = view.findViewById(R.id.tvAnnouncement)
+        tvAnnouncement = view.findViewById(R.id.tvAnnouncement)
+        etAnnouncement = view.findViewById(R.id.etAnnouncement)
+        btnEdit = view.findViewById(R.id.btnEdit)
+        btnCencel = view.findViewById(R.id.btnCencel)
+
+        etAnnouncement.isVisible = false
+        btnCencel.isClickable = false
 
         viewModel.getAnnouncement(companyId)
         viewModel.announcement.observe(viewLifecycleOwner, Observer {
             announcement_text = it
-            tvAnnoucement.setText(announcement_text)
+            tvAnnouncement.setText(announcement_text)
+            etAnnouncement.setText(announcement_text)
         })
 
 //        val userId = UserProfileFragmentArgs.fromBundle(requireArguments()).userId
@@ -56,6 +72,43 @@ class CompanyDashboardFragment : Fragment() {
         btn_back_main_menu.setOnClickListener {
             findNavController().popBackStack()
         }
+
+        btnEdit.setOnClickListener {
+            if(!nowedit){
+                etAnnouncement.isVisible = true
+                tvAnnouncement.isVisible = false
+                btnEdit.isClickable = false
+            }
+            else{
+                tvAnnouncement.setText(etAnnouncement.text)
+                etAnnouncement.isVisible = false
+                tvAnnouncement.isVisible = true
+            }
+        }
+
+        etAnnouncement.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable) {
+                // Ini akan dipanggil setelah teks berubah
+                println("Text changed to: ${s.toString()}")
+            }
+
+            override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {
+                // Ini akan dipanggil sebelum teks berubah
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
+                if(etAnnouncement.text != tvAnnouncement.text){
+                    btnEdit.setBackgroundResource(android.R.drawable.ic_menu_save)
+                    if(!btnEdit.isClickable){
+                        btnEdit.isClickable = true
+                    }
+                }
+                else{
+                    btnEdit.setBackgroundResource(android.R.drawable.ic_menu_edit)
+                    btnEdit.isClickable = false
+                }
+            }
+        })
 
         card_attendance.setOnClickListener {
             userId?.let { safeUserId ->
